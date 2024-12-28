1e8c096bf2349433974601bd34b3755353c58318
snowleaf-com <yy.dec5@gmail.com>
2024-12-29 07:58:46 +0900

    vuetifyインストール

--- src/sekkotuin/resources/js/app.ts
+++ src/sekkotuin/resources/js/app.ts
@@ -7,14 +7,21 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles';
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue') as any),
     setup({ el, app, props, plugin }) {
+        const vuetify = createVuetify({ components, directives });
         createApp({ render: () => h(app, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
