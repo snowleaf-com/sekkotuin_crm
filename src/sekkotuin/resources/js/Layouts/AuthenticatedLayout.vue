82064ce855ccfb5ed3450fa9e7be7c931f66d4bb
snowleaf-com <yy.dec5@gmail.com>
2024-12-29 13:14:08 +0900

    レイアウト大枠作成

--- src/sekkotuin/resources/js/Layouts/AuthenticatedLayout.vue
+++ src/sekkotuin/resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,152 +1,92 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+import { Inertia } from "@inertiajs/inertia";
 
-const showingNavigationDropdown = ref(false);
+const drawer = ref(false); // サイドバーの開閉状態を管理
+const navigationLinks = [
+  { name: "Dashboard", route: route("dashboard"), icon: "mdi-view-dashboard" },
+  // 必要に応じて他のリンクを追加
+];
+const logout = () => {
+    Inertia.post(route('logout'));
+}
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
+  <v-app>
+    <!-- ヘッダー -->
+    <v-app-bar color="white" elevated>
+      <!-- ハンバーガーメニュー -->
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
+      <!-- タイトル -->
+      <v-toolbar-title>
+        <Link :href="route('dashboard')">
+          <ApplicationLogo class="block h-12 w-auto fill-current text-gray-800" />
+        </Link>
+    </v-toolbar-title>
 
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {{ $page.props.auth.user.name }}
+      <!-- ユーザー情報 -->
+      <v-spacer />
+      <v-menu offset-y>
+        <template #activator="{ props }">
+          <v-btn v-bind="props" text>
+            {{ $page.props.auth.user.name }}
+            <v-icon right>mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+            <Link :href="route('profile.edit')" class="v-list-item cursor-pointer hover:bg-gray-100">
+                <v-list-item>
+                    <v-list-item-title>Profile</v-list-item-title>
+                </v-list-item>
+            </Link>
+            <Link @click="logout" class="v-list-item cursor-pointer hover:bg-gray-100">
+            <v-list-item>
+                <v-list-item-title>Log Out</v-list-item-title>
+            </v-list-item>
+            </Link>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
 
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
+    <!-- サイドバー -->
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <!-- サイドバーのナビゲーションリンク -->
+    <v-list>
+        <v-list-item
+          v-for="item in navigationLinks"
+          :key="item.name"
+          :active-class="'v-list-item--active'"
+          class="v-list-item"
+        >
+        <Link :href="item.route" class="v-list-item cursor-pointer hover:bg-gray-100 w-auto">
+            <v-icon class="mr-3">{{ item.icon }}</v-icon>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </Link>
+        </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
 
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-mr-2 flex items-center sm:hidden">
-                            <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div
-                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
-                    class="sm:hidden"
-                >
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
+    <!-- ページのコンテンツ -->
+    <v-main>
+      <!-- ヘッダー -->
+      <header class="bg-white shadow mt-4" v-if="$slots.header">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <slot name="header" />
         </div>
-    </div>
+      </header>
+
+      <!-- メインコンテンツ -->
+      <slot />
+    </v-main>
+  </v-app>
 </template>
+
+<style scoped>
+.v-list-item--active {
+  background-color: rgba(0, 0, 0, 0.08);
+}
+</style>
